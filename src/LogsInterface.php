6521f795d5b7ebc86a97db3bb8ec362622a54dff
Wolfy-J <wolfy.jd@gmail.com>
2018-08-20 13:39:00 +0300

    logs interface

--- src/LogsInterface.php
+++ src/LogsInterface.php
@@ -8,7 +8,18 @@
 
 namespace Spiral\Debug;
 
+use Psr\Log\LoggerInterface;
+
+/**
+ * LogsInterface is generic log factory interface.
+ */
 interface LogsInterface
 {
-
+    /**
+     * Get pre-configured logger instance.
+     *
+     * @param string $channel
+     * @return LoggerInterface
+     */
+    public function getLogger(string $channel): LoggerInterface;
 }
