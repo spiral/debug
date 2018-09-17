eedfb2837ad1ea331b605015154eeb4c10f8b970
Wolfy-J <wolfy.jd@gmail.com>
2018-09-17 20:21:51 +0300

    removed logs interface

--- tests/Debug/TraitsTest.php
+++ tests/Debug/TraitsTest.php
@@ -9,40 +9,16 @@
 namespace Spiral\Debug\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Psr\Log\NullLogger;
 use Spiral\Core\BootloadManager;
 use Spiral\Core\Container;
 use Spiral\Core\ContainerScope;
 use Spiral\Debug\Benchmarker;
 use Spiral\Debug\Bootloaders\DebugBootloader;
-use Spiral\Debug\LogsInterface;
 use Spiral\Debug\Traits\BenchmarkTrait;
-use Spiral\Debug\Traits\LoggerTrait;
 
 class TraitsTest extends TestCase
 {
-    use LoggerTrait, BenchmarkTrait;
-
-    public function testLoggerTrait()
-    {
-        $this->assertInstanceOf(NullLogger::class, $this->getLogger());
-        $this->logger = null;
-
-        $logger = new NullLogger();
-        $logs = $this->createMock(LogsInterface::class);
-        $logs->method('getLogger')->with(self::class)->willReturn($logger);
-
-        $c = new Container();
-        $c->bind(LogsInterface::class, $logs);
-
-        ContainerScope::runScope($c, function () use ($logger) {
-            $this->assertSame($logger, $this->getLogger());
-        });
-
-        $logger2 = new NullLogger();
-        $this->setLogger($logger2);
-        $this->assertSame($logger2, $this->getLogger());
-    }
+    use BenchmarkTrait;
 
     public function testBenchmarkTrait()
     {
